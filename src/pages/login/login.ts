51bc685354d6263d2d25a029dafca960dd5debc3
hosam1696 <hosamelnabawy@outlook.com>
2018-04-19 17:26:35 +0200

    login[signupPage]: edit the workflow in signup page
    * navigate to the login after a success register
    * display proper message to identify errors in email and password

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,99 +1,106 @@
-import { Component } from '@angular/core';
-import {IonicPage, NavController, ViewController, NavParams, Events} from 'ionic-angular';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import { Component } from "@angular/core";
+import {
+  IonicPage,
+  NavController,
+  ViewController,
+  NavParams,
+  Events
+} from "ionic-angular";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { UsersProviders } from "../../providers/users";
-import { AppUtilFunctions } from '../../providers/utilfuns';
-import {AppstorageProvider} from "../../providers/appstorage/appstorage";
+import { AppUtilFunctions } from "../../providers/utilfuns";
+import { AppstorageProvider } from "../../providers/appstorage/appstorage";
 
 @IonicPage()
 @Component({
-  selector: 'page-login',
-  templateUrl: 'login.html',
+  selector: "page-login",
+  templateUrl: "login.html"
 })
 export class LoginPage {
-    loginForm:FormGroup;
-    loader:boolean=false;
-    openAsPage; boolean = false;
-    constructor(
-        private appStorage: AppstorageProvider,
-        public appUtils: AppUtilFunctions,
-        public navCtrl: NavController,
-        private formBuilder: FormBuilder ,
-        public usersproviders: UsersProviders,
-        public viewCtrl: ViewController,
-        public events: Events,
-        navParams: NavParams
-    ) {
-     this.loginForm =this.formBuilder.group({
-         phone:['', Validators.required],
-         password:['', Validators.required],
-        });
+  loginForm: FormGroup;
+  loader: boolean = false;
+  openAsPage;
+  boolean = false;
+  constructor(
+    private appStorage: AppstorageProvider,
+    public appUtils: AppUtilFunctions,
+    public navCtrl: NavController,
+    private formBuilder: FormBuilder,
+    public usersproviders: UsersProviders,
+    public viewCtrl: ViewController,
+    public events: Events,
+    navParams: NavParams
+  ) {
+    this.loginForm = this.formBuilder.group({
+      phone: ["", Validators.required],
+      password: ["", Validators.required]
+    });
 
-        this.openAsPage = navParams.get('openAsPage')
+    this.openAsPage = navParams.get("openAsPage");
+  }
 
+  closePage() {
+    this.viewCtrl.dismiss();
+  }
+
+  goSignup() {
+    this.navCtrl.push("SignupPage");
+  }
+
+  toForgotPass() {
+    this.navCtrl.push("ResetpasswordPage");
+  }
+  goHomePage() {
+    this.navCtrl.push("HomePage");
+  }
+  onSubmit() {
+    if (this.loginForm.controls.phone.hasError("required")) {
+      this.appUtils.AppToast("الرجاء إدخال رقم الموبابل");
+    } else if (this.loginForm.controls.password.hasError("required")) {
+      this.appUtils.AppToast("الرجاء إدخال  كلمة المرور");
+    } else {
+      this.loader = true;
+
+      this.usersproviders.userLogin(this.loginForm.value).subscribe(
+        res => {
+          if (res.error) {
+            this.appUtils.AppToast("البيانات غير متطابقة");
+          } else {
+            console.log(res); // TODO: DEV Only reminder to be removed
+            this.appStorage.saveToken(res.token);
+            this.appUtils.AppToast("تم الدخول بنجاح");
+            this.usersproviders.getUserData(res.token).subscribe(data => {
+              console.log(data); // TODO: DEV Only reminder to be removed
+              if (data.user) {
+                data.user.token = res.token;
+                this.appStorage.saveUserData(data.user).then(() => {
+                  this.events.publish("refreshStorage");
+                  this.events.publish("changeRoot", "HomePage");
+                });
+              }
+            });
+          }
+        },
+        err => {
+          if (err.error instanceof ErrorEvent) {
+            console.info(
+              "%c%s%c%s",
+              "font-weight: bold;color:red;font-size:14px",
+              "Hosam",
+              "color: #333",
+              "  it's your fault check your code",
+              err
+            );
+          } else {
+            console.error(
+              `Backend returned code ${err.status}, ` +
+                `body was: ${err.error.body}`
+            );
+
+            this.appUtils.AppToast("خطأ فى الخادم");
+          }
+        }
+      );
     }
-
-    closePage() {
-        this.viewCtrl.dismiss();
-    }
-
-    goSignup() {
-        this.navCtrl.push("SignupPage");
-    }
-
-    toForgotPass() {
-        this.navCtrl.push('ResetpasswordPage')
-    }
-    goHomePage() {
-        this.navCtrl.push('HomePage')
-    }
-     onSubmit(){
-       if (this.loginForm.controls.phone.hasError('required')) {
-         this.appUtils.AppToast("الرجاء إدخال رقم الموبابل");
-       } else if (this.loginForm.controls.password.hasError('required')) {
-         this.appUtils.AppToast("الرجاء إدخال  كلمة المرور");
-       } else {
-         this.loader = true;
-
-         this.usersproviders
-           .userLogin(this.loginForm.value)
-           .subscribe((res) => {
-             if (res.error) {
-               this.appUtils.AppToast('البيانات غير متطابقة')
-             } else {
-               console.log(res); // TODO: DEV Only reminder to be removed
-               this.appStorage.saveToken(res.token);
-               this.appUtils.AppToast('تم الدخول بنجاح');
-               this.usersproviders.getUserData(res.token)
-                 .subscribe(data=>{
-                   console.log(data); // TODO: DEV Only reminder to be removed
-                   if (data.user) {
-                     data.user.name = data.user.first_name +' ' + data.user.last_name; // concat the user name
-                     data.user.token = res.token;
-                     this.appStorage.saveUserData(data.user)
-                       .then(()=>{
-                         this.events.publish('refreshStorage');
-                         this.events.publish('changeRoot','HomePage');
-                       });
-                   }
-                 })
-             }
-           },(err)=>{
-
-             if (err.error instanceof ErrorEvent) {
-               console.info('%c%s%c%s','font-weight: bold;color:red;font-size:14px','Hosam', 'color: #333' ,'  it\'s your fault check your code', err);
-             } else {
-               console.error(
-                 `Backend returned code ${err.status}, ` +
-                 `body was: ${err.error.body}`);
-
-               this.appUtils.AppToast('خطأ فى الخادم');
-             }
-
-
-           });
-       }
-     }
-
-
+  }
 }
--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import { Component } from "@angular/core";
 import {
   IonicPage,
   NavController,
@@ -7,23 +7,32 @@ import {
   Platform,
   ToastController,
   Events
-} from 'ionic-angular';
-import {FormBuilder, FormGroup, Validators, FormControl} from "@angular/forms";
-import {MyVariabels} from "../../providers/variables";
-import {AppUtilFunctions} from '../../providers/utilfuns';//
-import {UsersProviders} from "../../providers/users";
-import {HomePage} from '../home/home';
-import {Camera, CameraOptions} from '@ionic-native/camera';
-import {FilePath} from '@ionic-native/file-path';
-import {FileTransfer, FileUploadOptions, FileTransferObject,} from '@ionic-native/file-transfer';
-import {File} from '@ionic-native/file';
-import {AppstorageProvider} from "../../providers/appstorage/appstorage";
-import {UserData} from "../../providers/types/interface";
+} from "ionic-angular";
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl
+} from "@angular/forms";
+import { MyVariabels } from "../../providers/variables";
+import { AppUtilFunctions } from "../../providers/utilfuns"; //
+import { UsersProviders } from "../../providers/users";
+import { HomePage } from "../home/home";
+import { Camera, CameraOptions } from "@ionic-native/camera";
+import { FilePath } from "@ionic-native/file-path";
+import {
+  FileTransfer,
+  FileUploadOptions,
+  FileTransferObject
+} from "@ionic-native/file-transfer";
+import { File } from "@ionic-native/file";
+import { AppstorageProvider } from "../../providers/appstorage/appstorage";
+import { UserData } from "../../providers/types/interface";
 
 @IonicPage()
 @Component({
-  selector: 'page-signup',
-  templateUrl: 'signup.html',
+  selector: "page-signup",
+  templateUrl: "signup.html"
 })
 export class SignupPage {
   loader: boolean = false;
@@ -32,152 +41,159 @@ export class SignupPage {
   signupForm: FormGroup;
   usertypeInput: HTMLInputElement;
   formMasks: any = {
-    mobile: [/\d/, /\d/, /\d/, ' ', /\d/, /\d/, /\d/, ' ', /\d/, /\d/, /\d/]
+    mobile: [/\d/, /\d/, /\d/, " ", /\d/, /\d/, /\d/, " ", /\d/, /\d/, /\d/]
   };
 
-  constructor(public navCtrl: NavController,
-              public navParams: NavParams,
-              public formBuilder: FormBuilder,
-              public Vari: MyVariabels,
-              public appUtils: AppUtilFunctions,
-              public userProvider: UsersProviders,
-              public actionCtrl: ActionSheetController,
-              public camera: Camera,
-              public platform: Platform,
-              public file: File,
-              public filePath: FilePath,
-              public transfer: FileTransfer,
-              public toastCtrl: ToastController,
-              public appStorage: AppstorageProvider,
-              public events: Events,
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public formBuilder: FormBuilder,
+    public Vari: MyVariabels,
+    public appUtils: AppUtilFunctions,
+    public userProvider: UsersProviders,
+    public actionCtrl: ActionSheetController,
+    public camera: Camera,
+    public platform: Platform,
+    public file: File,
+    public filePath: FilePath,
+    public transfer: FileTransfer,
+    public toastCtrl: ToastController,
+    public appStorage: AppstorageProvider,
+    public events: Events
   ) {
     this.signupForm = this.formBuilder.group({
-      'role': ['user', Validators.required],
-      'first_name': ['', Validators.required],
-      'last_name': ['', Validators.required],
-      'avatar': [''],
-      'password': ['', Validators.required],
-      'confirm_password': ['', Validators.required],
-      'email': ['', [Validators.required, Validators.pattern(this.Vari.EMAIL_REGEXP)]],
-      'phone': ['', [Validators.required, Validators.pattern(this.Vari.NUMBER_REGXP), Validators.minLength(9), Validators.maxLength(9)]],
-      'address': ['', Validators.required],
-      'agreeCondition': [false, Validators.required],
-      'vehicle': [''],
-      'c_code': ['966']
-    })
+      role: ["user", Validators.required],
+      first_name: ["", Validators.required],
+      last_name: ["", Validators.required],
+      avatar: [""],
+      password: ["", Validators.required],
+      confirm_password: ["", Validators.required],
+      email: [
+        "",
+        [Validators.required, Validators.pattern(this.Vari.EMAIL_REGEXP)]
+      ],
+      phone: [
+        "",
+        [
+          Validators.required,
+          Validators.pattern(this.Vari.NUMBER_REGXP),
+          Validators.minLength(9),
+          Validators.maxLength(9)
+        ]
+      ],
+      address: ["", Validators.required],
+      agreeCondition: [false, Validators.required],
+      vehicle: [""],
+      c_code: ["966"]
+    });
   }
-
 
   submitForm() {
-    console.log(' this.signupForm.value ', this.signupForm.value);
-    if (this.signupForm.controls.first_name.hasError('required')) {
+    if (this.signupForm.controls.first_name.hasError("required")) {
       this.appUtils.AppToast("يرجى إدخال الاسم الاول");
-    }
-    else if (this.signupForm.controls.last_name.hasError('required')) {
+    } else if (this.signupForm.controls.last_name.hasError("required")) {
       this.appUtils.AppToast("يرجى إدخال الاسم الاخير");
-    }
-    else if (this.signupForm.controls.phone.hasError('required')) {
+    } else if (this.signupForm.controls.phone.hasError("required")) {
       this.appUtils.AppToast("ﻳﺮﺟﻰ ﺇﺩﺧﺎﻝ ﺭﻗﻢ اﻟﻤﻮﺑﺎﻳﻞ");
-    }
-
-    else if (this.signupForm.controls.email.hasError('required')) {
+    } else if (this.signupForm.controls.email.hasError("required")) {
       this.appUtils.AppToast("ﻳﺮﺟﻰ ﺇﺩﺧﺎﻝ اﻟﺒﺮﻳﺪ اﻹﻟﻜﺘﺮﻭﻧﻲ");
-    }
-    else if (this.signupForm.controls.email.hasError('pattern')) {
+    } else if (this.signupForm.controls.email.hasError("pattern")) {
       this.appUtils.AppToast("ﻳﺮﺟﻰ ﺇﺩﺧﺎﻝ اﻟﺒﺮﻳﺪ اﻹﻟﻜﺘﺮﻭﻧﻲ ﺑﺸﻜﻞ ﺻﺤﻴﺢ");
-    }
-    else if (this.signupForm.controls.address.hasError('required')) {
+    } else if (this.signupForm.controls.address.hasError("required")) {
       this.appUtils.AppToast("يرجى إدخال العنوان  ");
-    }
-    else if (this.signupForm.controls.password.hasError('required')) {
+    } else if (this.signupForm.controls.password.hasError("required")) {
       this.appUtils.AppToast("ﻳﺮﺟﻰ ﺇﺩﺧﺎﻝ ﻛﻠﻤﺔ اﻟﻤﺮﻭﺭ");
-    }
-    else if (this.signupForm.controls.confirm_password.hasError('required')) {
+    } else if (this.signupForm.controls.confirm_password.hasError("required")) {
       this.appUtils.AppToast("ﻳﺮﺟﻰ ﺇﺩﺧﺎﻝ ﺗﺄﻛﻴﺪ ﻛﻠﻤﺔ اﻟﻤﺮﻭﺭ ");
-    }
-    else if (this.signupForm.controls.password.value != this.signupForm.controls.confirm_password.value) {
+    } else if (
+      this.signupForm.controls.password.value !=
+      this.signupForm.controls.confirm_password.value
+    ) {
       this.appUtils.AppToast("ﻋﻔﻮا ﻛﻠﻤﺘﺎ اﻟﻤﺮﻭﺭ ﻏﻴﺮ ﻣﺘﻄﺎﺑﻘﺘﻴﻦ");
-    }
-    else if (this.signupForm.controls.agreeCondition.value == false) {
+    } else if (this.signupForm.controls.agreeCondition.value == false) {
       this.appUtils.AppToast("يرجى القراءة والموافقة على الشروط والاحكام");
-    }
-
-    else {
-      this.loader = true;
-      // customize the form object to be sent to the server
-      delete this.signupForm.value.agreeCondition;
-      delete this.signupForm.value.confirm_password;
-      this.signupForm.get('phone').setValue(this.signupForm.get('phone').value.replace(/\s/g, ''));
-      if (this.signupForm.get('role').value === 'user') {
-        this.signupForm.get('role').setValue('')
-      }
+    } else {
+      let signupData = {
+        ...this.signupForm.value,
+        name: this.concatWords("first_name", "last_name"),
+        phone: this.signupForm.get("phone").value.replace(/\s/g, "")
+      };
+      delete signupData.agreeCondition;
+      delete signupData.confirm_password;
 
       // send form object to the server
-      this.userProvider
-        .userRegister(this.signupForm.value)
-        .subscribe(async (res: UserData) => {
-          console.log(res);
+      this.userProvider.userRegister(signupData).subscribe(
+        async (res: UserData & { status: number; messages: string }) => {
+          console.log('Register Response',res); //TODO: reminder to remove this line
           if (res.id) {
-            this.appUtils.AppToast('تم التسجيل بنجاح');
-            await this.appStorage.saveToken(res.token);
-            res.name = res.first_name + ' ' + res.last_name; // concat the user name
+            this.appUtils.AppToast("تم التسجيل بنجاح");
             await this.appStorage.saveUserData(res);
-            this.events.publish('changeRoot', 'HomePage');
-            this.events.publish('refreshStorage');
-            this.navCtrl.setRoot('HomePage');
+            this.events.publish("changeRoot", "LoginPage");
+            this.events.publish("refreshStorage");
+            this.navCtrl.setRoot("LoginPage");
           } else {
-            console.log(res)
+            console.log('Register Response',res); //TODO: reminder to remove this line
+            if (res.status == 0) {
+              let msg =
+                res.messages === "this mail or phone already exists"
+                  ? "البريد الالكترونى او الهاتف موجودين بالفعل"
+                  : res.messages;
+              this.appUtils.AppToast(msg);
+            }
           }
-
-        }, err => {
+        },
+        err => {
           this.loader = false;
-          this.signupForm.get('role').setValue('user');
+          this.signupForm.get("role").setValue("user");
           console.warn(err);
-          this.appUtils.AppToast('خطأ فى الخادم');
-
-        }, () => {
+          this.appUtils.AppToast("خطأ فى الخادم");
+        },
+        () => {
           this.loader = false;
-        });
-
+        }
+      );
     }
-  }//end submit
+  } //end submit
 
-
-  changeUserType(event) {
-    this.signupForm.get('type_of_user').setValue(this.usertypeInput.value)
+  private concatWords(first: string, last: string): string {
+    return (
+      this.signupForm.get(first).value + " " + this.signupForm.get(last).value
+    );
   }
 
-  public pickImage() { // cameraImage defines we select to change (avatar | cover) image
+  changeUserType(event) {
+    this.signupForm.get("type_of_user").setValue(this.usertypeInput.value);
+  }
+
+  public pickImage() {
+    // cameraImage defines we select to change (avatar | cover) image
 
     let actionSheetCtrl = this.actionCtrl.create({
-      title: 'اختر من',
+      title: "اختر من",
       buttons: [
         {
-          text: 'الكاميرا',
+          text: "الكاميرا",
           handler: () => {
-            console.log('camera clicked');
+            console.log("camera clicked");
 
             /* open camera*/
-            this.openCamera('CAMERA');
-
+            this.openCamera("CAMERA");
           }
         },
         {
-          text: 'البوم الصور',
+          text: "البوم الصور",
           handler: () => {
-            console.log('Photo Album clicked');
+            console.log("Photo Album clicked");
 
             /*   open camera photo Library */
-            this.openCamera('PHOTOLIBRARY');
-
+            this.openCamera("PHOTOLIBRARY");
           }
         },
         {
-          text: 'الغاء',
-          role: 'cancel',
+          text: "الغاء",
+          role: "cancel",
           handler: () => {
-            console.log('Cancel clicked');
-
+            console.log("Cancel clicked");
           }
         }
       ]
@@ -185,10 +201,9 @@ export class SignupPage {
     actionSheetCtrl.present();
   }
 
-  private openCamera(type: string = 'CAMERA') {
-
+  private openCamera(type: string = "CAMERA") {
     const cameraOptions: CameraOptions = {
-      quality: (type == 'CAMERA') ? 70 : 40,
+      quality: type == "CAMERA" ? 70 : 40,
       destinationType: this.camera.DestinationType.FILE_URI,
       mediaType: this.camera.MediaType.PICTURE,
       correctOrientation: true,
@@ -196,60 +211,64 @@ export class SignupPage {
       sourceType: this.camera.PictureSourceType[type]
     };
 
-
     // returned File URI => file:///storage/emulated/0/Android/data/co.itplus.rf/cache/.Pic.jpg?1498042093580
     /* response
     {"bytesSent":176215,"responseCode":200,"response":"/home/httpprim/rfapp.net<br>/api/uploadImage.
       php<pre>Array\n(\n
       [0] => \n [1] => api\n [2] => uploadImage.php\n)\n/home/httpprim/rfapp.net<br>/api","objectId":""} */
 
-    this.camera.getPicture(cameraOptions).then(imageData => {
-
-      /* If data
+    this.camera
+      .getPicture(cameraOptions)
+      .then(imageData => {
+        /* If data
 
       let base64Image = 'data:image/jpeg;base64,' + imageData;
       let compressed = LZString.compressToUTF16(base64Image);
       console.log(compressed);
       */
-      console.log('line 171 on promise resolve function', imageData);
+        console.log("line 171 on promise resolve function", imageData);
 
-      // Special handling for Android library
-      if (this.platform.is('android') || type == 'PHOTOLIBRARY') {
-        this.filePath.resolveNativePath(imageData)
-          .then(filePath => {
-            console.log('file path from resolve native path', filePath);
-            let correctPath = filePath.substr(0, filePath.lastIndexOf('/') + 1);
-            let currentName = imageData.substring(imageData.lastIndexOf('/') + 1, imageData.lastIndexOf('?'));
-            console.log('correctPath', correctPath, 'currentName', currentName);
+        // Special handling for Android library
+        if (this.platform.is("android") || type == "PHOTOLIBRARY") {
+          this.filePath.resolveNativePath(imageData).then(filePath => {
+            console.log("file path from resolve native path", filePath);
+            let correctPath = filePath.substr(0, filePath.lastIndexOf("/") + 1);
+            let currentName = imageData.substring(
+              imageData.lastIndexOf("/") + 1,
+              imageData.lastIndexOf("?")
+            );
+            console.log("correctPath", correctPath, "currentName", currentName);
             //this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
           });
-      } else {
-        console.log('line 197 image file path', imageData);
-        let currentName = imageData.substr(imageData.lastIndexOf('/') + 1);
-        let correctPath = imageData.substr(0, imageData.lastIndexOf('/') + 1);
-        console.log('correctPath', correctPath, 'currentName', currentName);
-        //this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
-      }
+        } else {
+          console.log("line 197 image file path", imageData);
+          let currentName = imageData.substr(imageData.lastIndexOf("/") + 1);
+          let correctPath = imageData.substr(0, imageData.lastIndexOf("/") + 1);
+          console.log("correctPath", correctPath, "currentName", currentName);
+          //this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
+        }
 
+        // detect image extension
+        let extension: string = imageData.substring(
+          imageData.lastIndexOf(".") + 1,
+          imageData.lastIndexOf("?") != -1
+            ? imageData.lastIndexOf("?")
+            : imageData.length
+        );
 
-      // detect image extension
-      let extension: string = imageData.substring(imageData.lastIndexOf('.') + 1, imageData.lastIndexOf('?') != -1 ? imageData.lastIndexOf('?') : imageData.length);
+        console.log("file extension", extension);
 
-      console.log('file extension', extension);
+        // window.alert(imageData + "  && " + extension);
 
-      // window.alert(imageData + "  && " + extension);
-
-      return Promise.resolve([imageData, extension])
-
-    }).then(data => {
-
-      this.uploadImage(data[0], data[1]);
-
-    }).catch(err => {
-
-      console.error('getPicture Error ', err);
-      this.cameraError = err;
-    })
+        return Promise.resolve([imageData, extension]);
+      })
+      .then(data => {
+        this.uploadImage(data[0], data[1]);
+      })
+      .catch(err => {
+        console.error("getPicture Error ", err);
+        this.cameraError = err;
+      });
   }
 
   // private createFileName() {
@@ -260,64 +279,68 @@ export class SignupPage {
   // }
 
   private uploadImage(file, type) {
-    file = (file.indexOf('?') != -1) ? file.split('?')[0] : file;
+    file = file.indexOf("?") != -1 ? file.split("?")[0] : file;
 
     const fto: FileTransferObject = this.transfer.create();
 
-    let fileName = file.substr(file.lastIndexOf('/') + 1);
+    let fileName = file.substr(file.lastIndexOf("/") + 1);
 
     let uploadOptions: FileUploadOptions = {
-      fileKey: 'file',
+      fileKey: "file",
       fileName: fileName,
       chunkedMode: false,
-      mimeType: "image/" + type,
-
+      mimeType: "image/" + type
     };
 
-    let serverFile = this.Vari.API_URL + "uploadImage.php?uploadFolder=templates/default/uploads/avatars";
+    let serverFile =
+      this.Vari.API_URL +
+      "uploadImage.php?uploadFolder=templates/default/uploads/avatars";
 
     this.uploadLoader = true;
 
+    fto
+      .upload(encodeURI(file), encodeURI(serverFile), uploadOptions, true)
+      .then(
+        res => {
+          //this.loadImage = true;
+          this.showToast("جارى رفع الصورة");
+          console.log("uploaded", JSON.stringify(res));
+          this.uploadLoader = false;
+        },
+        err => {
+          //this.uploadErr = JSON.stringify(err);
+          //this.showToast('upload' + JSON.stringify(err));
+          console.log(err);
+          this.uploadLoader = false;
+          if (err.body) {
+            //this.showToast('image name ' + err.body);
+            console.log(
+              "%c%s",
+              "font-size:20px",
+              "Body message from the server",
+              err.body
+            );
+            console.log(JSON.parse(err.body), JSON.parse(err.body).name);
 
-    fto.upload(encodeURI(file), encodeURI(serverFile), uploadOptions, true)
-      .then((res) => {
-        //this.loadImage = true;
-        this.showToast('جارى رفع الصورة');
-        console.log('uploaded', JSON.stringify(res));
-        this.uploadLoader = false;
-      }, err => {
-        //this.uploadErr = JSON.stringify(err);
-        //this.showToast('upload' + JSON.stringify(err));
-        console.log(err);
-        this.uploadLoader = false;
-        if (err.body) {
-          //this.showToast('image name ' + err.body);
-          console.log('%c%s', 'font-size:20px', 'Body message from the server', err.body);
-          console.log(JSON.parse(err.body), JSON.parse(err.body).name);
-
-
-          //this.showToast(err.json().errorInfo());
-          this.showToast(JSON.parse(err.body).success)
-          if (JSON.parse(err.body).name) {
-            // this.userLocal[cameraImage] = JSON.parse(err.body).name;
-
-            // localStorage.setItem('userLocalData', JSON.stringify(this.userLocal));
-          } else {
-            this.showToast(JSON.parse(err.body).errorInfo)
+            //this.showToast(err.json().errorInfo());
+            this.showToast(JSON.parse(err.body).success);
+            if (JSON.parse(err.body).name) {
+              // this.userLocal[cameraImage] = JSON.parse(err.body).name;
+              // localStorage.setItem('userLocalData', JSON.stringify(this.userLocal));
+            } else {
+              this.showToast(JSON.parse(err.body).errorInfo);
+            }
           }
-
         }
-      });
-
+      );
   }
 
   public showToast(msg: string): void {
     let toast = this.toastCtrl.create({
       message: msg,
       duration: 3000,
-      position: 'top'
+      position: "top"
     });
     toast.present();
   }
-
 }
